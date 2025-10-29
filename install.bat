50b4584630dc022f4e40a51917fe00912246cb87
Caserol23 <gmcaserol@gmail.com>
2025-10-30 00:39:44 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -20,6 +20,9 @@ set reinstall_patch=%scriptbranch%\reinstall_all.bat
 set arkhelper=%scriptbranch%\tools\arkhelper.exe
 set buildark=%dirset%\bin\build\GEN
 set arkfolder=%dirset%\bin\_ark
+set libs=%dirset%\%gitout%
+
+set /p ps2dir="Locate PS2 Folder: "
 
 mkdir %dirset%
 cls
@@ -46,7 +49,7 @@ if not exist "%meyn%" (
 echo.
 echo Downloading Resources...
 echo.
-curl -L https://github.com/Caserol23/opmgh2dx_patcher/archive/refs/heads/main.zip --output %dirset%
+curl -L https://github.com/Caserol23/opmgh2dx_patcher/archive/refs/heads/main.zip --output %meyn%
 
 :proc
 "%sevenz%" x %meyn% -o"%dirset%" -y
@@ -59,50 +62,71 @@ echo Generating Scripts...
 echo.
 
 :: IML File
+mkdir %scriptbranch%
 echo Copying Scripts 1/4
 (
-[SYS]
-VERSION=1.00
-MEDIA=DVD
-TARGET=PS2
-DISCVERSION=1.00
-DATE=2025/10/29-23:10:27
-[/SYS]
+echo [SYS]
+echo VERSION=1.00
+echo MEDIA=DVD
+echo TARGET=PS2
+echo DISCVERSION=1.00
+echo DATE=2025/10/29-23:10:27
+echo [/SYS]
+echo.
+echo [CUE]
+echo DISCNAME="OPGH-12345                      "
+echo PRODUCER="PLAYSTATION                     "
+echo COPYRIGHT="                                "
+echo CREATIONDATE=20251029
+echo PSTYPE=2
+echo DISCCODE=02
+echo [/CUE]
+echo.
+echo [LOC]
+echo 0        291      0.0  0   "%geniml%\patch.ims"
+echo 292      324      0.0  0   "%geniml%\GEN\MAIN.HDR"
+echo 325      164059   0.0  0   "%geniml%\GEN\MAIN_0.ARK"
+echo 164060   164076   0.0  0   "%geniml%\IOP\CDVDSTM.IRX"
+echo 164077   164211   0.0  0   "%geniml%\IOP\IOPRP300.IMG"
+echo 164212   164236   0.0  0   "%geniml%\IOP\LGAUD.IRX"
+echo 164237   164251   0.0  0   "%geniml%\IOP\LIBSD.IRX"
+echo 164252   164302   0.0  0   "%geniml%\IOP\MCMAN.IRX"
+echo 164303   164306   0.0  0   "%geniml%\IOP\MCSERV.IRX"
+echo 164307   164310   0.0  0   "%geniml%\IOP\MSIFRPC.IRX"
+echo 164311   164316   0.0  0   "%geniml%\IOP\MTAPMAN.IRX"
+echo 164317   164339   0.0  0   "%geniml%\IOP\PADMAN.IRX"
+echo 164340   164340   0.0  0   "%geniml%\IOP\SCRTCHPD.IRX"
+echo 164341   164345   0.0  0   "%geniml%\IOP\SDRDRV.IRX"
+echo 164346   164348   0.0  0   "%geniml%\IOP\SIO2MAN.IRX"
+echo 164349   164416   0.0  0   "%geniml%\IOP\SYNTH_R.IRX"
+echo 164417   164433   0.0  0   "%geniml%\IOP\SYNTH_S.IRX"
+echo 164434   164451   0.0  0   "%geniml%\IOP\USBD.IRX"
+echo 164452   164458   0.0  0   "%geniml%\IOP\USBKB.IRX"
+echo 164459   166204   0.0  0   "%geniml%\OPGH_123.45"
+echo 166205   166205   0.0  0   "%geniml%\SYSTEM.CNF"
+echo 176447   176447   0.0  0   "%geniml%\patch.ims" 598016
+echo [/LOC]
+) > %geniml%
 
-[CUE]
-DISCNAME="OPGH-12345                      "
-PRODUCER="PLAYSTATION                     "
-COPYRIGHT="                                "
-CREATIONDATE=20251029
-PSTYPE=2
-DISCCODE=02
-[/CUE]
-
-[LOC]
-0        291      0.0  0   "%geniml%\patch.ims"
-292      324      0.0  0   "%geniml%\GEN\MAIN.HDR"
-325      164059   0.0  0   "%geniml%\GEN\MAIN_0.ARK"
-164060   164076   0.0  0   "%geniml%\IOP\CDVDSTM.IRX"
-164077   164211   0.0  0   "%geniml%\IOP\IOPRP300.IMG"
-164212   164236   0.0  0   "%geniml%\IOP\LGAUD.IRX"
-164237   164251   0.0  0   "%geniml%\IOP\LIBSD.IRX"
-164252   164302   0.0  0   "%geniml%\IOP\MCMAN.IRX"
-164303   164306   0.0  0   "%geniml%\IOP\MCSERV.IRX"
-164307   164310   0.0  0   "%geniml%\IOP\MSIFRPC.IRX"
-164311   164316   0.0  0   "%geniml%\IOP\MTAPMAN.IRX"
-164317   164339   0.0  0   "%geniml%\IOP\PADMAN.IRX"
-164340   164340   0.0  0   "%geniml%\IOP\SCRTCHPD.IRX"
-164341   164345   0.0  0   "%geniml%\IOP\SDRDRV.IRX"
-164346   164348   0.0  0   "%geniml%\IOP\SIO2MAN.IRX"
-164349   164416   0.0  0   "%geniml%\IOP\SYNTH_R.IRX"
-164417   164433   0.0  0   "%geniml%\IOP\SYNTH_S.IRX"
-164434   164451   0.0  0   "%geniml%\IOP\USBD.IRX"
-164452   164458   0.0  0   "%geniml%\IOP\USBKB.IRX"
-164459   166204   0.0  0   "%geniml%\OPGH_123.45"
-166205   166205   0.0  0   "%geniml%\SYSTEM.CNF"
-176447   176447   0.0  0   "%geniml%\patch.ims" 598016
-[/LOC]
-) > %geniml%"
+:: create menu
+(
+echo @echo off
+echo title OPM Guitar Hero 2 Deluxe Unified Patcher
+echo echo.
+echo echo ==========================================
+echo echo  OPM Guitar Hero 2 Deluxe Unified Patcher
+echo echo             Coded by Caserol23
+echo echo ==========================================
+echo.
+echo echo 1. Update Patch
+echo echo.
+echo echo ==========================================
+echo.
+echo echo 0. Exit
+echo set /p menunum=" > "
+echo if %menunum%=="1" start "%updaterecompile%"
+echo if %menunum%=="0" exit
+) > %scriptbranch%\opmgh2xu.bat
 
 :: optional scripts
 echo Copying Scripts 2/4
@@ -120,18 +144,14 @@ echo curl -L https://github.com/Caserol23/opmgh2dx_patcher/archive/refs/heads/ma
 cd %dirset%
 ren "%gitmain%" "%gitout%"
 echo.
-echo :: recompile files
+echo :: building ARK
 mkdir %buildark%
 echo "%arkhelper%" dir2ark "%buildark%" "%arkfolder%" -n MAIN -s 4073741823
-echo
+echo.
+echo :: building ISO
+echo.
+echo cd %libs%
+echo %libs%\python.exe main.py --iml=%geniml% --out_disc_image=%ps2dir%\OPMGH2DXU.iso
 ) > "%updaterecompile%"
 
-echo Copying Scripts 3/4
-(
-
-) > "%addcustompatch%"
-
-echo Copying Scripts 4/4
-(
-
-) > "%reinstall_patch%"
+pause
