a40f2df7be952771aab8d56ba8e8c67595f546c6
Caserol23 <gmcaserol@gmail.com>
2025-11-01 01:21:32 +0800

    a

--- install.bat
+++ install.bat
@@ -144,9 +144,6 @@ echo Copying Scripts 2/2
 (
 echo @echo off
 echo cls
-echo curl -L https://github.com/Caserol23/opmgh2dx_patcher/releases/download/song_patch/update.zip --output %TMP%\update.zip
-echo cls
-echo "%sevenz%" x %TMP%\update.zip -o"%arkfolder%\songs" -y
 echo %toolbranch%\arkhelper.exe dir2ark "%arkfolder%" "%buildark%\GEN" -n MAIN -s 4073741823
 echo %libs%\python.exe %libs%\main.py --iml=%buildiml% --out_disc_image="%ps2dir%\OPMGH2DXU.iso"
 echo cls
