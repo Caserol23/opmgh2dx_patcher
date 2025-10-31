2aaa5ea338d37227f39691e33d172c0fd7761ca1
Caserol23 <gmcaserol@gmail.com>
2025-10-31 23:52:56 +0800

    install and placeholder

--- install.bat
+++ install.bat
@@ -147,7 +147,6 @@ echo cls
 echo curl -L https://github.com/Caserol23/opmgh2dx_patcher/releases/download/song_patch/update.zip --output %TMP%\update.zip
 echo cls
 echo "%sevenz%" x %TMP%\update.zip -o"%arkfolder%\songs" -y
-echo type %arkfolder%\songs\songdta.txt ^>^> %sng%
 echo %toolbranch%\arkhelper.exe dir2ark "%arkfolder%" "%buildark%\GEN" -n MAIN -s 4073741823
 echo %libs%\python.exe %libs%\main.py --iml=%buildiml% --out_disc_image="%ps2dir%\OPMGH2DXU.iso"
 echo cls
